3a40d6690412c65840324f805c3f0de665c8ecab
Mburu Gathoni <paul.mburu@ngenx.io>
2026-05-13 16:52:08 +0300

    more update to appwrite api

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,18 +2,16 @@ module.exports = {
     apps: [
         {
             name: "appwrite-api",
-
             cwd: "C:/Users/PAUL/Desktop/angular_project/APPWRITE_API",
-
-            script: "C:/Users/PAUL/Desktop/angular_project/APPWRITE_API/env/Scripts/uvicorn.exe",
-
-            args: "-m uvicorn application.main:app --host 0.0.0.0 --port 8030",
-
-            interpreter: "none",
-
+            script: "run.py",
+            args: "application.main:app --host 0.0.0.0 --port 8030",
+            interpreter: "C:/Users/PAUL/Desktop/angular_project/APPWRITE_API/env/Scripts/python.exe",
             watch: false,
-
-            autorestart: true
+            autorestart: true,
+            env: {
+                FLASK_ENV: "development",
+                FLASK_DEBUG: "1"
+            }
         }
     ]
 }
